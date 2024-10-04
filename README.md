ad36d6a34f6fef1d0f79a1985455148a51f68365
jfgulden <guldenjf@gmail.com>
2024-10-03 22:34:19 -0300

    agregar descripcion de la complejidad

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-hola
+# Trabajo Práctico TDA
+
+## Integrantes
+
+- Theo Miguel
+- Brayan Ricaldi
+- Franco Altiera
+- Daniela Gómez
+- Juan Francisco Gulden
--- excercise1.py
+++ excercise1.py
@@ -4,11 +4,9 @@ def obtener_ganador(monedas):
     # Turno Sophia: Agarra la moneda más grande
     # Turno Mateo: Agarra la moneda más chica
     inicio = 0
-    set_monedas = set(monedas)
-    print(len(set_monedas))
     fin = len(monedas) - 1
     monedas_sofia = []
-    monedas_mateo = []
+
     for i in range(len(monedas)): # len(monedas): Turnos totales
         primera = monedas[inicio]
         ultima = monedas[fin]
@@ -21,39 +19,41 @@ def obtener_ganador(monedas):
                 fin -= 1
         else:
             if primera >= ultima:
-                monedas_mateo.append(ultima)
                 fin -= 1
             else:
-                monedas_mateo.append(primera)
                 inicio += 1
 
 
-    return sum(monedas_sofia), sum(monedas_mateo)
+    return sum(monedas_sofia)
 
-def leer_archivo(file: str):
+def obtener_monedas_de_archivo(file: str):
     file = open(file, "r")
     file.readline()
     monedas = list(map(int, file.readline().strip().split(';')))
     return monedas
 
-
-monedas = leer_archivo("archivos_pruebas/20.txt")
+"""
+Complejidad: 
+    - Temporal: O(n), siendo n la cantidad total de monedas.
+    - Espacial: O(n), siendo n la cantidad total de monedas.
+"""
+monedas = obtener_monedas_de_archivo("archivos_pruebas/20.txt")
 print(obtener_ganador(monedas))
 
-monedas = leer_archivo("archivos_pruebas/25.txt")
+monedas = obtener_monedas_de_archivo("archivos_pruebas/25.txt")
 print(obtener_ganador(monedas))
 
-monedas = leer_archivo("archivos_pruebas/50.txt")
+monedas = obtener_monedas_de_archivo("archivos_pruebas/50.txt")
 print(obtener_ganador(monedas))
 
-monedas = leer_archivo("archivos_pruebas/100.txt")
+monedas = obtener_monedas_de_archivo("archivos_pruebas/100.txt")
 print(obtener_ganador(monedas))
 
-monedas = leer_archivo("archivos_pruebas/1000.txt")
+monedas = obtener_monedas_de_archivo("archivos_pruebas/1000.txt")
 print(obtener_ganador(monedas))
 
-monedas = leer_archivo("archivos_pruebas/10000.txt")
+monedas = obtener_monedas_de_archivo("archivos_pruebas/10000.txt")
 print(obtener_ganador(monedas))
 
-monedas = leer_archivo("archivos_pruebas/20000.txt")
+monedas = obtener_monedas_de_archivo("archivos_pruebas/20000.txt")
 print(obtener_ganador(monedas))

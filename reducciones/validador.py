0a500efda6620a78a00be127e951bd54c3f4da94
jfgulden <guldenjf@gmail.com>
2024-10-28 22:42:41 -0300

    algunos tests y solucion de bug

--- reducciones/validador.py
+++ reducciones/validador.py
@@ -1,9 +1,9 @@
 
 '''
 Requisitos:
-- No se pueden poner los barcos de forma adyacente.
+- No pueden haber barcos adyacentes.
 - Los barcos tienen un ancho de una celda.
-- Se deben cumplir con las restricciones para filas y columnas.
+- Se debe cumplir con las restricciones para filas y columnas.
 - Se deben colocar todos los barcos.
 '''
 from typing import List
@@ -23,6 +23,31 @@ def validate_restrictions(board: List[List[bool]], boats: List[int], res_rows: L
 
     return True
 
+def validate_diagonal(board: List[List[bool]], i: int, j: int):
+    if i+1 < len(board) and j+1 < len(board[0]):
+        if board[i+1][j+1]:
+            return False
+
+    return True
+
+def validate_adjacency_right(board: List[List[bool]], i: int, j: int):
+    
+    if i+1 < len(board):
+        if board[i+1][j]:
+            return False
+
+    return validate_diagonal(board, i, j)
+
+def validate_adjacency_down(board: List[List[bool]], i: int, j: int):
+    
+    if j+1 < len(board[0]):
+        if board[i][j+1]:
+            return False
+
+    return validate_diagonal(board, i, j)
+
+
+
 def validate_boat(board: List[List[bool]], boats, i: int, j: int):
     boat_size = 1
     board[i][j] = False
@@ -33,13 +58,9 @@ def validate_boat(board: List[List[bool]], boats, i: int, j: int):
                 break
             board[i][k] = False
             boat_size += 1
-
-            if row_index+1 < len(board) and board[row_index+1][k]:
+            
+            if not validate_adjacency_right(board, row_index, k):
                 return False
-
-            if row_index+1 < len(board) and k+1 < len(board[0]) and board[row_index+1][k+1]:
-                return False
-
             row_index += 1
 
     if i+1 < len(board) and board[i+1][j]:
@@ -50,16 +71,13 @@ def validate_boat(board: List[List[bool]], boats, i: int, j: int):
             board[k][j] = False
             boat_size += 1
 
-            if col_index+1 < len(board[0]) and board[k][col_index+1]:
+            if not validate_adjacency_down(board, k, col_index):
                 return False
 
-            if k+1 < len(board) and col_index+1 < len(board[0]) and board[k+1][col_index+1]:
-                return False
-            
             col_index += 1
 
     if boat_size == 1:
-        if i+1 < len(board) and j+1 < len(board[0]) and board[i+1][j+1]: # Diagonal derecha
+        if not validate_diagonal(board, i, j):
             return False
     
     if boat_size in boats:
@@ -82,10 +100,59 @@ def naval_battle_validator(board: List[List[bool]], boats: [int], res_rows: [int
                 if not validate_boat(board, boats, i, j):
                     return False
 
+    if len(boats) > 0:  # Si no se colocaron todos los barcos
+        return False
+
     return True
                 
 
 # Test cases
+board = [
+    [True, False, False, False, True],
+    [False, False, False, False, True],
+    [False, False, False, False, True],
+    [False, True, True, False, True]
+]
+boats = [1, 2, 4]
+res_rows = [2,1,1,3]
+res_cols = [1,1,1,0,4]
+print(naval_battle_validator(board, boats, res_rows, res_cols)) # True
+
+board = [
+    [True, False, False, False, True],
+    [False, True, False, False, True],
+    [False, False, False, False, True],
+    [False, True, True, False, True]
+]
+boats = [1, 2, 4]
+res_rows = [2,2,1,3]
+res_cols = [1,2,1,0,4]
+print(naval_battle_validator(board, boats, res_rows, res_cols)) # False: Hay un barco en diagonal al [0,0]
+
+
+board = [
+    [True, False, False, False, True],
+    [False, True, False, False, True],
+    [False, False, False, False, True],
+    [False, True, True, False, True]
+]
+boats = [1, 2, 4]
+res_rows = [2,2,1,3]
+res_cols = [1,2,1,0,5]
+print(naval_battle_validator(board, boats, res_rows, res_cols)) # False: No se cumplen las restricciones de columnas
+
+board = [
+    [True, False, False, False, True],
+    [False, True, False, False, True],
+    [False, False, False, False, True],
+    [False, True, True, False, True]
+]
+boats = [1, 2, 4]
+res_rows = [2,2,1,4]
+res_cols = [1,2,1,0,4]
+print(naval_battle_validator(board, boats, res_rows, res_cols)) # False: No se cumplen las restricciones de filas
+
+
 board = [
     [True, False, False, False, True],
     [False, True, False, False, True],
@@ -93,7 +160,21 @@ board = [
     [False, True, True, False, True]
 ]
 boats = [1, 2, 4, 1]
-res_rows = [2,2,1,3]
+res_rows = [2,2,1,4]
 res_cols = [1,2,1,0,4]
+print(naval_battle_validator(board, boats, res_rows, res_cols)) # False: Queda un barco sin poner
+
+board = [
+    [True, False, False, False, True],
+    [False, False, True, False, True],
+    [False, False, False, False, True],
+    [False, True, True, False, True],
+    [False, False, False, False, False]
+]
+boats = [1, 2, 4, 1]
+res_rows = [2,2,1,3,0]
+res_cols = [1,1,2,0,4]
 print(naval_battle_validator(board, boats, res_rows, res_cols)) # True
+
+#TODO: Mejorar estos tests
 

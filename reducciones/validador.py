abfdcf89b19942f6280030c09992ab56d36e7a3d
jfgulden <guldenjf@gmail.com>
2024-10-28 23:01:19 -0300

    refactor de validador

--- reducciones/validador.py
+++ reducciones/validador.py
@@ -47,41 +47,52 @@ def validate_adjacency_down(board: List[List[bool]], i: int, j: int):
     return validate_diagonal(board, i, j)
 
 
+def search_boat_to_right(board: List[List[bool]], i: int, j: int, boat_size: List[int]):
+    row_index = i   # Para verificar si hay barcos adyacentes
+    for k in range(j, len(board[0])):
+        if not board[i][k]:
+            break
+        board[i][k] = False
+        boat_size[0] += 1
+        
+        if not validate_adjacency_right(board, row_index, k):
+            return False
+        row_index += 1
+
+    return True
+
+def search_boat_to_bottom(board: List[List[bool]], i: int, j: int, boat_size: List[int]):
+    col_index = j   # Para verificar si hay barcos adyacentes
+    for k in range(i, len(board)):
+        if not board[k][j]:
+            break
+        board[k][j] = False
+        boat_size[0] += 1
+
+        if not validate_adjacency_down(board, k, col_index):
+            return False
+        col_index += 1
+
+    return True
+
 
 def validate_boat(board: List[List[bool]], boats, i: int, j: int):
-    boat_size = 1
+    boat_size = [1]
     board[i][j] = False
     if j+1 < len(board[0]) and board[i][j+1]:
-        row_index = i   # Para verificar si hay barcos adyacentes
-        for k in range(j+1, len(board[0])):
-            if not board[i][k]:
-                break
-            board[i][k] = False
-            boat_size += 1
-            
-            if not validate_adjacency_right(board, row_index, k):
-                return False
-            row_index += 1
+        if not search_boat_to_right(board, i, j+1, boat_size):
+            return False
 
     if i+1 < len(board) and board[i+1][j]:
-        col_index = j   # Para verificar si hay barcos adyacentes
-        for k in range(i+1, len(board)):
-            if not board[k][j]:
-                break
-            board[k][j] = False
-            boat_size += 1
-
-            if not validate_adjacency_down(board, k, col_index):
-                return False
-
-            col_index += 1
+        if not search_boat_to_bottom(board, i+1, j, boat_size):
+            return False
 
     if boat_size == 1:
         if not validate_diagonal(board, i, j):
             return False
     
-    if boat_size in boats:
-        boats.remove(boat_size)
+    if boat_size[0] in boats:
+        boats.remove(boat_size[0])
         return True
     
     return False
@@ -89,7 +100,7 @@ def validate_boat(board: List[List[bool]], boats, i: int, j: int):
 
 
     
-def naval_battle_validator(board: List[List[bool]], boats: [int], res_rows: [int], res_cols: [int]):
+def naval_battle_validator(board: List[List[bool]], boats: List[int], res_rows: List[int], res_cols: List[int]):
 
     if not validate_restrictions(board, boats, res_rows, res_cols):
         return False
@@ -177,4 +188,5 @@ res_cols = [1,1,2,0,4]
 print(naval_battle_validator(board, boats, res_rows, res_cols)) # True
 
 #TODO: Mejorar estos tests
+#TODO: Analizar complejidad -> Explicar por que es polinomial 
 

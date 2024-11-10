70c1675479bea2e850497586df6819a1281facda
jfgulden <guldenjf@gmail.com>
2024-11-10 15:48:24 -0300

    refactor bt

--- excercise_3/backtracking.py
+++ excercise_3/backtracking.py
@@ -1,9 +1,14 @@
 from typing import List
 from typing import Tuple
 import numpy as np
+from enum import Enum
+
+class Orientacion(Enum):
+    Horizontal = 1
+    Vertical = 2
 
 
-def validar_diagonal(tablero: List[List[int]], i: int, j: int):
+def validar_diagonal(tablero: List[List[int]], i: int, j: int) -> bool:
     if i+1 < len(tablero) and j+1 < len(tablero[0]):
         if tablero[i+1][j+1]:
             return False
@@ -24,14 +29,14 @@ def validar_diagonal(tablero: List[List[int]], i: int, j: int):
     return True
 
 
-def puede_colocar_horizontalmente(tablero: List[List[int]], len_barco: int, demandas_filas: List[int], demandas_columnas: List[int], i: int, j: int):
-    if j + len_barco - 1 >= m:
+def puede_colocar_horizontalmente(tablero: List[List[int]], len_barco: int, demandas_filas: List[int], demandas_columnas: List[int], i: int, j: int) -> bool:
+    n = len(tablero)
+    m = len(tablero[0])
+
+    if j + len_barco - 1 >= m or demandas_filas[i] < len_barco:
         return False
 
-    if demandas_filas[i] < len_barco:
-        return False
-
-    if j - 1 >= 0 and tablero[i][j-1]: # Valido que no haya ningun barco en la posicion anterior
+    if j - 1 >= 0 and tablero[i][j-1] or j + len_barco < m and tablero[i][j + len_barco]:
         return False
 
     for k in range(j, j + len_barco):
@@ -41,25 +46,20 @@ def puede_colocar_horizontalmente(tablero: List[List[int]], len_barco: int, dema
         if not validar_diagonal(tablero, i, k):
             return False
 
-        if i+1 < n and tablero[i+1][k]:
+        if i+1 < n and tablero[i+1][k] or i-1 >= 0 and tablero[i-1][k]:
             return False
-        if i-1 >= 0 and tablero[i-1][k]:
-            return False
-
-    if j + len_barco < m and tablero[i][j + len_barco]: # Valido que no haya ningun barco en la posicion siguiente
-        return False
 
     return True
 
 
-def puede_colocar_verticalmente(tablero: List[List[int]], len_barco: int, demandas_filas: List[int], demandas_columnas: List[int], i: int, j: int):
-    if i + len_barco - 1 >= n:
+def puede_colocar_verticalmente(tablero: List[List[int]], len_barco: int, demandas_filas: List[int], demandas_columnas: List[int], i: int, j: int) -> bool:
+    n = len(tablero)
+    m = len(tablero[0])
+
+    if i + len_barco - 1 >= n or demandas_columnas[j] < len_barco:
         return False
 
-    if demandas_columnas[j] < len_barco:
-        return False
-
-    if i - 1 >= 0 and tablero[i-1][j]: # Valido que no haya ningun barco en la posicion anterior
+    if i - 1 >= 0 and tablero[i-1][j] or i + len_barco < n and tablero[i+len_barco][j]:
         return False
 
     for k in range(i, i + len_barco):
@@ -69,65 +69,57 @@ def puede_colocar_verticalmente(tablero: List[List[int]], len_barco: int, demand
         if not validar_diagonal(tablero, k, j):
             return False
 
-        if j+1 < m and tablero[k][j+1]:
-            return False
-
-        if j-1 >= 0 and tablero[k][j-1]:
-            return False
-
-    if i + len_barco < n and tablero[i+len_barco][j]: # Valido que no haya ningun barco en la posicion siguiente
+        if j+1 < m and tablero[k][j+1] or j-1 >= 0 and tablero[k][j-1]:
             return False
         
     return True
 
-def puede_colocar_barco(tablero: List[List[int]], len_barco: int, demandas_filas: List[int], demandas_columnas: List[int], i: int, j: int, orientacion: str):
-    n = len(tablero)
-    m = len(tablero[0])
+def puede_colocar_barco(tablero: List[List[int]], len_barco: int, demandas_filas: List[int], demandas_columnas: List[int], i: int, j: int, orientacion: Orientacion) -> bool:
     if tablero[i][j]:
         return False
 
-    if orientacion == "Horizontal":
+    if orientacion == Orientacion.Horizontal:
         return puede_colocar_horizontalmente(tablero, len_barco, demandas_filas, demandas_columnas, i, j)
         
-    if orientacion == "Vertical":
+    if orientacion == Orientacion.Vertical:
         return puede_colocar_verticalmente(tablero, len_barco, demandas_filas, demandas_columnas, i, j)
     
     return False
             
 
-def colocar_barco(tablero: List[List[int]], len_barco: int, i: int, j: int, orientacion: str):
+def colocar_barco(tablero: List[List[int]], len_barco: int, i: int, j: int, orientacion: Orientacion):
 
-    if orientacion == "Horizontal":
+    if orientacion == Orientacion.Horizontal:
         for k in range(j, j + len_barco):
             tablero[i][k] = 1
 
-    if orientacion == "Vertical":
+    if orientacion == Orientacion.Vertical:
         for k in range(i, i + len_barco):
             tablero[k][j] = 1
             
-def remover_barco(tablero: List[List[int]], len_barco: int, i: int, j: int, orientacion: str):
+def remover_barco(tablero: List[List[int]], len_barco: int, i: int, j: int, orientacion: Orientacion):
 
-    if orientacion == "Horizontal":
+    if orientacion == Orientacion.Horizontal:
         for k in range(j, j + len_barco):
             tablero[i][k] = 0
 
-    if orientacion == "Vertical":
+    if orientacion == Orientacion.Vertical:
         for k in range(i, i + len_barco):
             tablero[k][j] = 0
             
-def actualizar_demandas(len_barco: int, i: int, j: int, orientacion: str, demandas_filas: List[int], demandas_columnas: List[int]):
+def actualizar_demandas(len_barco: int, i: int, j: int, orientacion: str, demandas_filas: List[int], demandas_columnas: List[int]) -> Tuple[List[int], List[int]]:
     n = len(demandas_filas)
     m = len(demandas_columnas)
     
     demandas_filas = demandas_filas.copy()
     demandas_columnas = demandas_columnas.copy()
     
-    if orientacion == "Horizontal":
+    if orientacion == Orientacion.Horizontal:
         for k in range(j, j + len_barco):
             demandas_filas[i] -= 1
             demandas_columnas[k] -= 1
 
-    if orientacion == "Vertical":
+    if orientacion == Orientacion.Vertical:
         for k in range(i, i + len_barco):
             demandas_filas[k] -= 1
             demandas_columnas[j] -= 1
@@ -147,21 +139,20 @@ def batalla_naval_BT(tablero: List[List[int]], barcos: List[int], demandas_filas
         return
 
     barco = barcos[barco_actual]
-    barco_largo = barco
     
-    for orientacion in ['Horizontal', 'Vertical']:
+    for orientacion in Orientacion:
         for i in range(n):
             for j in range(m):
-                if puede_colocar_barco(tablero, barco_largo, demandas_filas, demandas_columnas, i, j, orientacion):
-                    colocar_barco(tablero, barco_largo, i, j, orientacion)
-                    nueva_demanda_filas, nueva_demanda_columnas = actualizar_demandas(barco_largo, i, j, orientacion, demandas_filas, demandas_columnas)
+                if puede_colocar_barco(tablero, barco, demandas_filas, demandas_columnas, i, j, orientacion):
+                    colocar_barco(tablero, barco, i, j, orientacion)
+                    nueva_demanda_filas, nueva_demanda_columnas = actualizar_demandas(barco, i, j, orientacion, demandas_filas, demandas_columnas)
                     batalla_naval_BT(tablero, barcos, nueva_demanda_filas, nueva_demanda_columnas, mejor_solucion, barco_actual+1)
-                    remover_barco(tablero, barco_largo, i, j, orientacion)
+                    remover_barco(tablero, barco, i, j, orientacion)
     
     # Intentar omitir el barco actual
     batalla_naval_BT(tablero, barcos, demandas_filas, demandas_columnas, mejor_solucion, barco_actual+1)
 
-def batalla_naval(tablero: List[List[int]], barcos: List[int], demandas_filas: List[int], demandas_columnas: List[int]):
+def batalla_naval(tablero: List[List[int]], barcos: List[int], demandas_filas: List[int], demandas_columnas: List[int]) -> Tuple[List[List[int]], int]:
     mejor_solucion = [None]
     batalla_naval_BT(tablero, barcos, demandas_filas, demandas_columnas, mejor_solucion)
     return mejor_solucion[0]
